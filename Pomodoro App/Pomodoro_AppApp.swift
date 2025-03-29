5ec8b5d3f9bb0aae8147512bdae7ec8773c6a133
iamadlet <adlet.zhumagaliev2003@gmail.com>
2025-03-29 13:52:00 +0100

    Initial commit

---
+++ Pomodoro App/Pomodoro_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_AppApp.swift
+//  Pomodoro App
+//
+//  Created by Адлет Жумагалиев on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
