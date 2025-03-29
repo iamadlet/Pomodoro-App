b065fbdb4a171983b1b82c2ff0f005ab46d724d2
iamadlet <adlet.zhumagaliev2003@gmail.com>
2025-03-29 14:23:46 +0100

    Create model

---
+++ Pomodoro App/Model/DailyHistory.swift
@@ -0,0 +1,8 @@
+//
+//  DailyHistory.swift
+//  Pomodoro App
+//
+//  Created by Адлет Жумагалиев on 29.03.2025.
+//
+
+import Foundation
---
+++ Pomodoro App/Model/FocusCategory.swift
@@ -0,0 +1,8 @@
+//
+//  FocusCategory.swift
+//  Pomodoro App
+//
+//  Created by Адлет Жумагалиев on 29.03.2025.
+//
+
+import Foundation
