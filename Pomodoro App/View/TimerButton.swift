ba1eb4d4bd52445964ca6060ccda8ea41b944120
iamadlet <adlet.zhumagaliev2003@gmail.com>
2025-04-04 16:24:03 +0200

    Update

---
+++ Pomodoro App/View/TimerButton.swift
@@ -0,0 +1,18 @@
+//
+//  TimerButton.swift
+//  Pomodoro App
+//
+//  Created by Адлет Жумагалиев on 01.04.2025.
+//
+
+import SwiftUI
+
+struct TimerButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerButton()
+}
